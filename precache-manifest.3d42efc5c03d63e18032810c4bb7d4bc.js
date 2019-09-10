8899d1b5034aeee8718142d906cf625b45de8942
Email Update <haojinzhao@yahoo.com>
2019-09-10 16:31:34 -0500

    Updates

---
+++ precache-manifest.3d42efc5c03d63e18032810c4bb7d4bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00058210a0f87487b90019d33f7ad5c",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "5af36188b2429d73eb5c",
+    "url": "/clicky-game/static/css/main.a01b7602.chunk.css"
+  },
+  {
+    "revision": "a11dabe951d5a17d9deb",
+    "url": "/clicky-game/static/js/2.2745d238.chunk.js"
+  },
+  {
+    "revision": "5af36188b2429d73eb5c",
+    "url": "/clicky-game/static/js/main.3172971d.chunk.js"
+  },
+  {
+    "revision": "79ea0d5502c3a6ac0f74",
+    "url": "/clicky-game/static/js/runtime~main.80bf01e5.js"
+  }
+]);
